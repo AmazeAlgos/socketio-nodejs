c02d8f3e9795c8fadc3cebcd8f098b8e7c469262
AmazeAlgos <102874035+AmazeAlgos@users.noreply.github.com>
2024-09-04 12:33:39 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 8000
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html')
+})
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
